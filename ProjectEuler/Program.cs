ab887b08c253be913d696e5acaca0ee2f2769a05
Zachary Carey <zach.carey@outlook.com>
2021-01-16 17:09:18 -0500

    Puzzle0018

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectEuler {
 	class Program {
 
 		static void Main(string[] args) {
-			Puzzle CurrentPuzzle = new Puzzle0017();
+			Puzzle CurrentPuzzle = new Puzzle0018();
 
 			if (!Debugger.IsAttached) {
 				CurrentPuzzle = null;
---
+++ ProjectEuler/Puzzles/Puzzle0018.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ProjectEuler.Common;
+
+namespace ProjectEuler.Puzzles {
+
+	/// <summary>
+	/// A class representing the solution to <c>https://projecteuler.net/problem=18</c>.
+	/// </summary>
+	sealed class Puzzle0018 : Puzzle {
+
+		/// <inheritdoc/>
+		public override string Question => "Find the maximum total from top to bottom of the triangle below:";
+
+		int[][] triangle;
+
+		/// <inheritdoc/>
+		public override object Solve() {
+			loadData();
+			return findMax(0, 0);
+		}
+
+		private int findMax(int x, int y) {
+			//Base cases
+			if (y >= triangle.Length) return 0;
+			if (x >= triangle[y].Length) return 0;
+
+			int left = findMax(x, y + 1);
+			int right = findMax(x + 1, y + 1);
+			return triangle[y][x] + Math.Max(left, right);
+		}
+
+		private void loadData() {
+			List<int[]> data = new List<int[]>();
+
+			foreach(string line in ReadResourceLines()) {
+				data.Add(line.Trim().Split().Select(int.Parse).ToArray());
+			}
+
+			triangle = data.ToArray();
+		}
+	}
+}
