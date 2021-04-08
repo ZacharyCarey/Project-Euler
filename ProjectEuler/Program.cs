e77c82e63babdecc2adfabca39335fd5cd51528f
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-04-08 11:33:55 -0400

    Puzzle0025

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectEuler {
 	class Program {
 
 		static void Main(string[] args) {
-			Puzzle CurrentPuzzle = new Puzzle0024();
+			Puzzle CurrentPuzzle = new Puzzle0025();
 
 			if (!Debugger.IsAttached) {
 				CurrentPuzzle = null;
