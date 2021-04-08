cb6837365ded8b4a25f46c58344264b2add6c950
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-04-08 14:47:44 -0400

    Setup for next problem

---
+++ ProjectEuler/Puzzles/Puzzle0027.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ProjectEuler.Common;
+
+namespace ProjectEuler.Puzzles {
+
+	/// <summary>
+	/// A class representing the solution to <c>https://projecteuler.net/problem=27</c>.
+	/// </summary>
+	sealed class Puzzle0027 : Puzzle {
+
+		/// <inheritdoc/>
+		public override string Question => "Find the product of the coefficients,  and , for the quadratic expression that produces the maximum number of primes for consecutive values of , starting with .";
+
+		/// <inheritdoc/>
+		public override object Solve() {
+			return null;
+		}
+	}
+}
