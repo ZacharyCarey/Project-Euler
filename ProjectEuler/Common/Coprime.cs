28f8055ed3b7204f3e39ea7359f1e8fa805f68f3
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-04-08 14:46:02 -0400

    Puzzle0026

---
+++ ProjectEuler/Common/Coprime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler.Common {
+	public partial class Utils {
+
+		public static IEnumerable<int> Coprime10() {
+			//We check for i > 0 so we can detect when the overflow happens to allow the generator to go all the way up to int.MaxValue
+			for(int i = 3; i > 0; i+=2) {
+				if(i % 5 != 0) {
+					yield return i;
+				}
+			}
+		}
+
+		public static bool IsCoprime10(this int n) {
+			if (n < 2) return false;
+			if (n % 2 == 0) return false;
+			if (n % 5 == 0) return false;
+
+			return true;
+		}
+
+	}
+}
---
+++ ProjectEuler/Puzzles/Puzzle0026.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ProjectEuler.Common;
+
+namespace ProjectEuler.Puzzles {
+
+	/// <summary>
+	/// A class representing the solution to <c>https://projecteuler.net/problem=26</c>.
+	/// </summary>
+	sealed class Puzzle0026 : Puzzle {
+
+		/// <inheritdoc/>
+		public override string Question => "Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.";
+
+		/// <inheritdoc/>
+		public override object Solve() {
+			//Lots of info was learned from Eli Bendersky's website - see Math Docs
+
+			//"To find the longest cycle it's sufficient to look just at the primes - because other numbers will just have the same cycles after some initial non-repetitive section."
+			int maxLen = 0;
+			int maxN = 0;
+			foreach(int prime in Utils.Primes.TakeWhile(x => x < 1000)) {
+				int rest = 1;
+				for(int i = 0; i < prime; i++) {
+					rest = (rest * 10) % prime;
+				}
+
+				int r0 = rest;
+				int len = 0;
+				do {
+					rest = (rest * 10) % prime;
+					len++;
+				} while (rest != r0);
+
+				if(len > maxLen) {
+					maxLen = len;
+					maxN = prime;
+				}
+			}
+
+			return maxN;
+		}
+	}
+}
